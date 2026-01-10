4a7a60f6b49206c62e8918583bdbbc4885cbe5cb
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-01-10 23:05:11 +0530

    Initial commit

---
+++ src/main/java/com/atithiai/AtithiAiApplication.java
@@ -0,0 +1,14 @@
+package com.atithiai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtithiAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtithiAiApplication.class, args);
+		System.out.println("project running.");	
+	}
+
+}
---
+++ src/test/java/com/atithiai/AtithiAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atithiai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtithiAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
