32f90d6a9851288a5b3cc53d01207b6548a4e60f
Tanvi jagtap <tanvijagtap910@gmail.com>
2026-01-12 20:46:46 +0530

    Create MenuItemService.java

---
+++ src/main/java/com/atithiai/services/MenuItemService.java
@@ -0,0 +1,35 @@
+package com.atithiai.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.atithiai.entities.MenuItem;
+import com.atithiai.repositories.MenuItemRepository;
+
+@Service
+public class MenuItemService {
+	
+	private final MenuItemRepository menuItemRepository;
+
+    public MenuItemService(MenuItemRepository menuItemRepository) {
+        this.menuItemRepository = menuItemRepository;
+    }
+
+    public MenuItem addMenuItem(MenuItem menuItem) {
+        return menuItemRepository.save(menuItem);
+    }
+
+    public List<MenuItem> getAllMenuItems() {
+        return menuItemRepository.findAll();
+    }
+
+    public MenuItem updateMenuItem(MenuItem menuItem) {
+        return menuItemRepository.save(menuItem);
+    }
+
+    public void deleteMenuItem(Long id) {
+        menuItemRepository.deleteById(id);
+    }
+
+}
